fabec28aa2457620836cb29f423fe7bc5e31172b
Ismavreverse <Ismavreverse@users.noreply.github.com>
2017-10-13 13:04:57 +0530

    Update BasicMathematicalOperations.js

--- BasicMathematicalOperations.js
+++ BasicMathematicalOperations.js
@@ -1,17 +1,12 @@
-function basicOp(operation, value1, value2)
-{
+function basicOp(operation, value1, value2) {
   var result = 0;
-  if(operation === '+'){
-    result = value1+value2;
-  }
-  else if(operation === '-'){
-    result = value1-value2;
-  }
-  else if(operation === '*'){
-    result = value1*value2;
-  }
-  else if(operation === '/'){
-    result = value1/value2;
-  }
+  if(operation === '+') {
+    result = value1 + value2;
+  } else if (operation === '-'){
+    result = value1 - value2;
+  } else if (operation === '*'){
+    result = value1 * value2;
+  } else if(operation === '/'){
+    result = value1 / value2;}
   return result;
 }
