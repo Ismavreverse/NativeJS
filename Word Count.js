7b79427bcfc2d9fbbde9799d659dffd4fcd7b472
Ismavreverse <Ismavreverse@users.noreply.github.com>
2017-10-24 17:44:20 +0530

    Delete Word Count.js

--- Word Count.js
+++
@@ -1,8 +0,0 @@
-function countWords(str) {
-  return str
-     .trim()
-     .replace( /\s+/g , ' ') //we are using regular expression so it should work with non-whitespace(ex. breakspace) chars"﻿Hello﻿World "
-     .split(' ')
-     .filter(function(n) { return n != '' })
-     .length;
-}
