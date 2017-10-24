cc75e795f55b2864c2da1c608f48c0c62c9db65f
Ismavreverse <Ismavreverse@users.noreply.github.com>
2017-10-24 17:41:30 +0530

    Delete BasicMathematicalOperations.js

--- BasicMathematicalOperations.js
+++
@@ -1,9 +0,0 @@
-function basicOp(operation, value1, value2) {
-  var OBJ = { 
-     '+' : function(value1, value2) { return value1 + value2; },
-     '-' : function(value1, value2) { return value1 - value2; },
-     '*' : function(value1, value2) { return value1 * value2; },
-     '/' : function(value1, value2) { return value1 / value2; }
-  }
-  return OBJ[operation](value1, value2);
-}
