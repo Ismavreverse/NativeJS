6716319d1bd62d162d6189c8be8962df3c489ef9
Ismavreverse <Ismavreverse@users.noreply.github.com>
2017-10-25 11:38:39 +0530

    Working..

--- Session 2/Partition On.js
+++ Session 2/Partition On.js
@@ -1,17 +1,11 @@
-// partition the items array so that all values for which pred returns true are
-// at the end, returning the index of the first true value
-
 function partitionOn(pred, items) {
-
-   var evenitems=[],odditems=[]
+   var evenitems=[],odditems=[];
    for (i = 0; i < items.length; i++) { 
      pred(items[i])?evenitems.push(items[i]):odditems.push(items[i]);
    }
-
-items.sort(function(a, b) {
-  return b % 2 - a % 2
-});
+   items.sort(function(a, b) {
+      return b % 2 - a % 2
+   });
    //items = odditems.concat(evenitems);
    return odditems.length;
 }
-
