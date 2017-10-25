49ad167a2404a798a0fe1ebf4817d90ad0110af4
Ismavreverse <Ismavreverse@users.noreply.github.com>
2017-10-25 11:39:07 +0530

    Working..

--- Session 2/Partition On.js
+++ Session 2/Partition On.js
@@ -1,7 +1,7 @@
 function partitionOn(pred, items) {
    var evenitems=[],odditems=[];
    for (i = 0; i < items.length; i++) { 
-     pred(items[i])?evenitems.push(items[i]):odditems.push(items[i]);
+     pred(items[i]) ? evenitems.push(items[i]) : odditems.push(items[i]);
    }
    items.sort(function(a, b) {
       return b % 2 - a % 2
