48ee4e1c2fdcf074a46d78993757f59ddfdb8845
Ismavreverse <Ismavreverse@users.noreply.github.com>
2017-10-26 12:37:31 +0530

    Update Partition On.js

--- Session 2/Partition On.js
+++ Session 2/Partition On.js
@@ -1,11 +1,11 @@
 function partitionOn(pred, items) {
-   var evenitems=[],odditems=[];
-   for (i = 0; i < items.length; i++) { 
+  var evenitems=[],odditems=[];
+  for (i = 0; i < items.length; i++) { 
      pred(items[i]) ? evenitems.push(items[i]) : odditems.push(items[i]);
-   }
-   items.sort(function(a, b) {
-      return b % 2 - a % 2
-   });
-   //items = odditems.concat(evenitems);
-   return odditems.length;
+  }
+  var updated = odditems.concat(evenitems);
+  for (var i=0;i<updated.length;i++){
+    items[i] = updated[i];
+  }
+   return odditems.length;  
 }
