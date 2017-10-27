ea2df7c165efb7c0eaaf19f6e584ee7a381a3090
Ismavreverse <Ismavreverse@users.noreply.github.com>
2017-10-27 11:09:57 +0530

    Update Prefill an Array.js

--- Session 3/Prefill an Array.js
+++ Session 3/Prefill an Array.js
@@ -1,7 +1,9 @@
 function prefill(n, v) {
   try {
+    //Array.apply(null, new Array(parseInt(n, 10))) --- here will consider null value to the Array having decimal values of size 10
     var arr = Array.apply(null, new Array(parseInt(n, 10)));
-    return arr.map(function() {
+    //The map() method creates a new array with the results of calling a provided function on every element in the calling array.
+    return arr.map(function() { 
         return v;
     });
   } catch (e) {
