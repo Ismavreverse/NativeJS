bf36b4b58921c9820dc1137c2bdea4e02b31c578
Ismavreverse <Ismavreverse@users.noreply.github.com>
2017-11-05 22:35:34 +0530

    Update Prefill an Array.js

--- Session 3/Prefill an Array.js
+++ Session 3/Prefill an Array.js
@@ -1,7 +1,8 @@
 function prefill(n, v) {
-  try {
-    return Array(parseInt(n, 10)).fill(v);
-  } catch (e) {
-    throw new TypeError(n+' is invalid');
-  }   
+  //parseInt considers the integer (till it reaches a letter or symbol or decimal point) of the given string
+  //Math.abs takes only positive numbers or null
+  if ( parseInt(n) !== Math.abs(n) ) {
+    throw new TypeError( n + ' is invalid' );
+  }  
+  return Array(parseInt(n)).fill(v);
 }
