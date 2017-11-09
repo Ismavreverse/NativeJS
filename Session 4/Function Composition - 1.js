68227fe674414f119942affa35f703175dcfae4b
Ismavreverse <Ismavreverse@users.noreply.github.com>
2017-11-09 08:03:18 +0530

    Update Function Composition - 1.js

--- Session 4/Function Composition - 1.js
+++ Session 4/Function Composition - 1.js
@@ -1,7 +1,11 @@
 function compose(f,g) {
+  return function() {
+     return f(g.apply(this, arguments));
+  };
+}
+ -- ALTERNATE SOLUTION --
 //if any parameter say 'n' here has ... as prefix then it is considered as an array statring from 0 to <array.length 
 //and this is rest parameter syntax
    return function(...n) {
         return f(g(...n));
     }
-}
