8c2bc9b291b5f08d33c6111c2dec71d3c5d7faee
Ismavreverse <Ismavreverse@users.noreply.github.com>
2017-11-12 23:43:14 +0530

    Create SantaClausable Interface.js

---
+++ Session 5/SantaClausable Interface.js
@@ -0,0 +1,7 @@
+function isSantaClausable(obj) {
+  if( !obj.sayHoHoHo || !obj.distributeGifts || !obj.goDownTheChimney || 
+  typeof(obj.sayHoHoHo) === "boolean" || typeof(obj.distributeGifts) === "boolean" || typeof(obj.goDownTheChimney) === "boolean")
+      return false;
+  else 
+      return true;
+}
